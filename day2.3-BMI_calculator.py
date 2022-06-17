45309da98b00ea3f06b6b297bb5f29668017661b
prajapatsur <100709956+prajapatsur@users.noreply.github.com>
2022-06-17 15:40:39 +0530

    Add files via upload

---
+++ day2.3-BMI_calculator.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+w=float(weight)
+h=float(height)
+BMI = w/(h*h)   #BMI= w/(h**2)
+print(BMI)
+#Write your code below this line 👇
